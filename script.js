0441b95a9c0045954d0ac1071c3882d10936e802
Ana Julia Boneti Volgue <ana.volgue@escola.pr.gov.br>
2025-11-10 14:12:49 +0000

    arvore pronta

---
+++ script.js
@@ -0,0 +1,9 @@
+function acender(){
+    document.getElementById("arvore").src="images.jpeg"
+    console.log("on")
+}
+
+function apagar(){
+    document.getElementById("arvore").src="desenho5.png"
+     console.log("off")
+}
